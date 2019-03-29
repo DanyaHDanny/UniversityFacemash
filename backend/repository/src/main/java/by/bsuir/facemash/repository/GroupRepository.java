cdaa64d5e57b835a46050f9db90c98ab153fd2cf
NikitaUstyushenko <nikita09061999@mail.ru>
2019-03-29 18:23:45 +0300

    Create repository for entities Group and Faculty

---
+++ backend/repository/src/main/java/by/bsuir/facemash/repository/GroupRepository.java
@@ -0,0 +1,13 @@
+package by.bsuir.facemash.repository;
+
+import by.bsuir.facemash.entity.Group;
+import by.bsuir.facemash.repository.api.RepositoryApi;
+
+/**
+ * Rpository interface for etnity {@link Group}.
+ *
+ * @author Mikita_Ustsiushenka
+ * @version 1.0
+ */
+public interface GroupRepository extends RepositoryApi<Group> {
+}
